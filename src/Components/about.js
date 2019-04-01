77bff7c64cad2aaaaec31e6d7bee8794a8dc8d0a
Vsevolod Brovarnyi <vsevolod.brovarnyi@synapse.com>
2019-04-01 16:38:04 +0300

    add about block

--- src/Components/about.js
+++ src/Components/about.js
@@ -6,35 +6,13 @@ const About = () => {
     return (
             <div className="content">
                 <ScrollAnimation animateIn="flipInY" animateOnce={true}>
-                    <h3 className="content-title">Что такое L&B</h3>     
+                    <h3 className="content-title small-margin">Что такое L&B</h3>     
                 </ScrollAnimation>                
-                <div className="content-area">
-                    <ScrollAnimation animateIn="bounceInUp" animateOnce={true} delay={100}>
-                        <div className="content-area--block">
-                            <p className="big-text">
-                                <span>200 000</span>
-                                <span>Мастеров</span>
-                            </p>
-                            <p>Именно такое количество мастеровприсоеденилось к нашему сообществу, стань частью большой команды.</p>
-                        </div>
-                    </ScrollAnimation>
-                    <ScrollAnimation animateIn="bounceInUp" animateOnce={true} delay={200}>
-                        <div className="content-area--block">
-                            <p className="big-text">
-                                <span>15 000</span>
-                                <span>Клиентов.</span>
-                            </p>
-                            <p>Среднее количество кол-во клиентов которые ежедневно пользуються услугами мастеров.</p>
-                        </div>
-                    </ScrollAnimation>
-                    <ScrollAnimation animateIn="bounceInUp" animateOnce={true} delay={300}>
-                        <div className="content-area--block">
-                            <p className="big-text">
-                                <span>5 000</span>
-                                <span>/ мес</span>
-                            </p>
-                            <p>Наша большая команда растет с каждым месяцем, Все больше мастеров по всему миру выберают нас.</p>
-                        </div>
+                <div className="content-area about">
+                    <ScrollAnimation animateIn="bounceInUp" animateOnce={true}>
+                        <p>L&B это важнейший инcтрумент для твоего бизнеса, который <b>поможет найти новых клиентов</b>, 
+                            получить <b>бесплатно нужные рекомендации</b> от лучших экспертов западного рынка, <b>повысить уровень своей квалификации</b>,
+                            получить признание и даже <b>стать одним из экспертов европейских брендов</b>. Становись лучшим вместе с нами!</p>
                     </ScrollAnimation>
                 </div>          
             </div>
