5be5c803bb16d5c8e5136379e3a1df1da5f91f5a
Vsevolod Brovarnyi <vsevolod.brovarnyi@synapse.com>
2019-04-22 17:14:43 +0300

    Add translation to meta tegs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from "axios";
+//for SEO
+import {Helmet} from "react-helmet";
 
 import translateData from './Components/HOC/translate';
 
@@ -53,7 +55,14 @@ componentDidMount() {
 
   render() {    
     return (
-      <div className="main">      
+      <div className="main">    
+        <Helmet>
+            <meta charSet="utf-8" />            
+            <meta name="description" content={this.state.lang === 'eng' ? "Convenient service that combines manicure masters and customers." : "Удобный сервис который объединяет мастеров маникюра и клиентов."}/>
+            <meta name="keywords" content={this.state.lang === 'eng'  ? "manicure, shilak, the best manicure masters, pedicure" : "маникюр, шилак, лучшие мастера маникюра, педикюр"}/>
+            <link rel="canonical" href="http://lnb.life/index.html" />
+            <title>{this.state.lang === 'eng' ? 'Life & Beauty - the best masters and clients with us': 'Life & Beauty - лучшие мастера и клиенты у нас'}</title>
+        </Helmet>  
         <PopUp open={this.state.popUp} translateData={this.state.lang === 'eng' ? translateData.eng : translateData.rus}/>
         <Header handlePopUp = {this.handlePopUp} translateData={this.state.lang === 'eng' ? translateData.eng : translateData.rus}/>
         <div className="main-content">
